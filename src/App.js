f6f01acd43088074daf442af93401a12aa95f1a1
armaBurton <armanarma@armaburton.com>
2022-02-28 15:18:50 -0800

    starting user login and authentication

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  NavLink,
+} from 'react-router-dom';
+import { getUser, logout, getOwnage } from './services/fetch-utils';
+import { useEffect, useState } from 'react';
 import './App.css';
+// import userEvent from '@testing-library/user-event';
+import AuthPage from './AuthPage/AuthPage';
 
 function App() {
+  const [user, setUser] = useState();
+  
+
+
+  console.log(`|| user >`, user);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {
+          !user
+            ? <header></header>
+            : <header>
+              <ul>
+                <li className='nav-link'><NavLink to='#'>One</NavLink></li>
+                <li className='nav-link'><NavLink to='#'>Two</NavLink></li>
+                <li className='nav-link'><NavLink to='#'>Three</NavLink></li>
+              </ul>
+            </header>
+        }
+        <main>
+          <Switch>
+            <Route exact path='/'>
+              {
+                !user
+                  ? <AuthPage setUser={setUser} />
+                  : <Redirect to='#' user={user} />
+              }
+            </Route>
+            <Route>
+              {
+
+              }
+            </Route>
+            <Route></Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
 }
 
