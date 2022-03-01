f0ab9f87fd6a8e8a28c69ba951b36e8c28b25393
Benatovich <sambenatovich@gmail.com>
2022-02-28 21:07:33 -0500

    Game.js is a work in progress. fetchWord function is mostly written

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
               {
                 !user
                   ? <AuthPage setUser={setUser} />
-                  : <Redirect to='/game' user={user} />
+                  : <Redirect to='/game' />
               }
             </Route>
             <Route>
--- src/Game/Game.js
+++ src/Game/Game.js
@@ -1,9 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useGameContext } from '../GameProvider';
 import Row from '../Row/Row';
+import commonWords from '../common-words';
 
 export default function Game() {
   const { correctWord, setCorrectWord, guessedWord, setGuessedWord } = useGameContext();
+  
+  useEffect(() => {
+    function fetchWord() {
+      const index = Math.floor(Math.random() * commonWords.length);
+
+      setCorrectWord(commonWords[index]);
+    }
+  
+    fetchWord();
+  }, []);
+
 
   return (
     <form>
