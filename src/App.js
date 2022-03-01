29a31bbad5281c2ee5ca53e6839fd5d347948cc3
Philippe Ngom <philngom@gmail.com>
2022-02-28 16:39:57 -0800

    logout function in App.js working

--- src/App.js
+++ src/App.js
@@ -14,6 +14,11 @@ import AuthPage from './AuthPage/AuthPage';
 function App() {
   const [user, setUser] = useState();
 
+  async function handleLogout() {
+    await logout();
+    setUser(null);
+  }
+
   return (
     <Router>
       <div>
@@ -25,6 +30,9 @@ function App() {
                 <li className='nav-link'><NavLink to='#'>One</NavLink></li>
                 <li className='nav-link'><NavLink to='#'>Two</NavLink></li>
                 <li className='nav-link'><NavLink to='#'>Three</NavLink></li>
+                <li>
+                  <button onClick={ handleLogout }>Logout</button>
+                </li>
               </ul>
             </header>
         }
