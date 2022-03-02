8a58f2e4414c813d10a0afc6523a17143a8da4a1
Philippe Ngom <philngom@gmail.com>
2022-03-02 09:49:19 -0800

    input rendering in correct squares

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -24,37 +24,37 @@ export default function Game() {
       const index = Math.floor(Math.random() * commonWords.length);
       setCorrectWord(commonWords[index]);
     }
-    
+
     fetchWord();
-    
+
     // setGameState();
   }, []);
 
-  
 
-  function setGameState(currentGuess, e) {
-    e.preventDefault();
-    // as you type, this function should split the letters up into an array, save the array in state, and populate the boxes with the corresponding letters from the array
-    // const eachRow = new Array(correctWord.length);
-    // const newGameArray = game.map(() => eachRow);
-    // setGame(newGameArray);
-    const guessArray = currentGuess.split('');
 
+  function setGameState(input) {
+    let guessArray = input.split('');
+    while (guessArray.length < correctWord.length) {
+      guessArray.push('');
+    }
+    game[row] = guessArray;
+    setGame([...game]);
   }
 
   function handleGuess(e) {
+    e.preventDefault();
     // on submit, this function should compare the array from setGameState to the array of correctWord.split() and change tile colors accordingly
     // e => setGuessedWord(e.target.value)
 
-  } 
+  }
 
   return (
     <div className="entire-game">
-      <form>
-        <input className='invisible-guess' autoFocus onChange={e => setGameState(e.target.value)} maxLength={correctWord.length} onSubmit={e => handleGuess(e)} />
+      <form onSubmit={e => handleGuess(e)}>
+        <input className='invisible-guess' autoFocus onChange={e => setGameState(e.target.value)} maxLength={correctWord.length}/>
       </form>
       {
-        game.map((row, i) => <Row key={row + i} y={i} />)
+        game.map((currentRow, i) => <Row currentRow={currentRow} key={currentRow + i} y={i} />)
       }
     </div>
   );
